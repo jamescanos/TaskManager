f45631fb7b85b7573a99128b8e43bbf883f32f34
Jhon James Cano Sanchez <jhonjamescanosanchez@gmail.com>
2026-07-13 13:16:44 -0500

    ajustes Render

--- Dockerfile
+++ Dockerfile
@@ -42,4 +42,4 @@ COPY ./entrypoint.sh /usr/local/bin/entrypoint.sh
 RUN chmod +x /usr/local/bin/entrypoint.sh
 
 EXPOSE 80
-ENTRYPOINT ["/usr/local/bin/entrypoint.sh"]
+CMD ["sh", "/usr/local/bin/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,9 @@ php artisan view:cache
 echo "Ejecutando migraciones de base de datos..."
 php artisan migrate --force
 
+# CORRECCIÓN IMPORTANTE: Iniciar PHP-FPM en segundo plano (Modo Demonio)
+php-fpm -D
+
 # Arrancar el servidor web Nginx en primer plano
+echo "Iniciando Nginx..."
 nginx -g 'daemon off;'
